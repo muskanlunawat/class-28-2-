2e5bbb05191df1c0bcc0c4f7a173dfe12cd7a3e9
muskanlunawat <76555879+muskanlunawat@users.noreply.github.com>
2021-02-09 16:04:45 +0530

    Add files via upload

---
+++ SlingShot.js
@@ -0,0 +1,35 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.4,
+            length: 10
+        }
+        this.pointB = pointB
+        this.SlingShot= Constraint.create(options);
+        World.add(world, this.SlingShot);
+    }
+
+        fly(){
+            this.SlingShot.bodyA = null;
+
+        }
+
+        display(){
+            if(this.SlingShot.bodyA){
+                
+
+            
+            var pointA = this.SlingShot.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            }
+        
+    }
+    
+}
+
+
+
